1b4239c044ecc15d19c0ba25250b21ce959fcd63
dhq-github2019 <dhq-github2019>
2024-12-15 22:21:17 +0700

    change API endpoint

--- udagram-frontend/src/environments/environment.ts
+++ udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://a7541021299824c2abe9e6d78eb08f07-538356936.us-east-1.elb.amazonaws.com/api/v0:8080/api/v0'
 };
 
 /*
